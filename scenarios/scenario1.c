d0469e332e5beba2087b34b259629069bf649a41
Ferdinando Romano <fmromano@vt.edu>
2013-09-18 19:52:52 -0400

    functioning scenario1

--- scenarios/scenario1.c
+++ scenarios/scenario1.c
@@ -5,29 +5,38 @@
 #include <liquid/liquid.h>
 
 // CRTS Header Files
-#include "scenarios/scenario1.h"
+//#include "scenario1.h"
+//#include "scenarios/scenario1.h"
+
 
 void scenario1(float complex * transmit_buffer, unsigned int symbol_len)
 {
+
     //options
     float dphi  = 0.001f;                   // carrier frequency offset
     float SNRdB = 20.0f;                    // signal-to-noise ratio [dB]
-
+    
     // noise parameters
     float nstd = powf(10.0f, -SNRdB/20.0f); // noise standard deviation
     float phi = 0.0f;                       // channel phase
 
     int last_symbol=0;
+    complex float buffer[symbol_len]; 
 
-    unsigned int i;
-
+    unsigned int i=0;
     // noise mixing
     for (i=0; i<symbol_len; i++) {
         transmit_buffer[i] *= cexpf(_Complex_I*phi); // apply carrier offset
         phi += dphi;                        // update carrier phase
         cawgn(&buffer[i], nstd);            // add noise
     }
-    
-    return 0;
+    //return 0;
 }
 	
+void main()
+{
+float complex a[300] ;
+unsigned int b = 100;
+
+scenario1(a,b);
+}
