4560c40bbce8684d0df758cc989dd4494de144a0
Ferdinando Romano <fmromano@vt.edu>
2013-09-26 14:52:50 -0400

    latest version of crts - not done yet

--- crts.c
+++ crts.c
@@ -32,6 +32,10 @@ struct CognitiveEngine {
     float latestGoalValue;
     int iterations;
     int payloadLen;
+    unsigned int numSubcarriers;
+    unsigned int CPLen;
+    unsigned int taperLen;
+
 }
 
 struct Scenario {
@@ -60,7 +64,12 @@ struct CognitiveEngine CreateCognitiveEngine() {
         .latestGoalValue = 0.0;           // Value of goal to be compared to threshold
         .iterations = 100;                // Number of transmissions made before attemting to receive them.
         .payloadLen = 120;                      // Length of payload in frame generation
+        .numSubcarriers = 64;               // Number of subcarriers for OFDM
+        .CPLen = 16;                        // Cyclic Prefix length
+        .taperLen = 4;                      // Taper length
     }
+    // TODO: Call function to read config file and change specified parameters. e.g.
+    // ReadCEConfig(&ce);
     return ce;
 }
 
@@ -75,6 +84,8 @@ struct Scenario CreateScenario() {
 
         .addFading = 0;
     }
+    // TODO: Call function to read config file and change specified parameters. e.g.
+    // ReadScConfig(&ce);
     return sc;
 }
 
@@ -94,7 +105,7 @@ ofdmflexframegen CreateFG(struct CognitiveEngine ce, struct Scenario sc) {
         //TODO: Skip current test if given an unkown parameter.
     }
 
-    // TODO: Have all parameters be set by ce and se objects.
+    // TODO: Have all other parameters be set by ce and se objects as well.
     
     // Frame generation parameters
     crc_scheme check = LIQUID_CRC_32;
@@ -108,10 +119,19 @@ ofdmflexframegen CreateFG(struct CognitiveEngine ce, struct Scenario sc) {
     fgprops.fec0            = fec0;
     fgprops.fec1            = fec1;
     fgprops.mod_scheme      = ms;
-    ofdmflexframegen fg = ofdmflexframegen_create(M, cp_len, taper_len, NULL, &fgprops);
+    ofdmflexframegen fg = ofdmflexframegen_create(ce.numSubcarriers, ce.CPLen, ce.taperLen, NULL, &fgprops);
 
     return fg;
 }
+
+// TODO: Once we are using USRPs, move to an rx.c file that will run independently.
+ofdmflexframesync CreateFS(struct CognitiveEngine ce, struct Scenario sc)
+{
+     ofdmflexframesync fs =
+             ofdmflexframesync_create(ce.numSubCarriers, ce.CPLen, ce.taper_len, NULL, rxCallback, NULL);
+
+     return fs;
+} // End CreateFS();
 
 // Transmit a packet of data.
 // This will need to be modified once we implement the USRPs.
@@ -122,17 +142,30 @@ int txGeneratePacket(struct CognitiveEngine ce, ofdmflexframegen * _fg, char * h
     //char payload[ce.payloadLen];
 
     // Generate data
-    printf("\n\n\ngenerating data that will go in frame...\n");
+    printf("\n\ngenerating data that will go in frame...\n");
     for (i=0; i<8; i++)
     header[i] = i & 0xff;
     for (i=0; i<payload_len; i++)
     payload[i] = rand() & 0xff;
 
     // Assemble frame
-    ofdmflexframegen_assemble(fg, header, payload, ce.payloadLen);
+    ofdmflexframegen_assemble(*_fg, header, payload, ce.payloadLen);
 
     return 1;
+} // End txGeneratePacket()
+
+int txTransmitPacket(struct CognitiveEngine ce, ofdmflexframegen * _fg, float complex * frameSamples)
+{
+    int isLastSymbol = ofdmflexframegen_writesymbol(*fg, samples_buffer);
+    return isLastSymbol;
 } // End txTransmitPacket()
+
+int rxReceivePacket(struct CognitiveEngine ce, ofdmflexframesyc * _fs, float complex * frameSamples)
+{
+    unsigned int symbolLen = ce.numSubcarriers + ce.CPLen;
+    ofdmflexframesync_execute(*fs, frameSamples, symbolLen);
+    return 1;
+} // End rxReceivePacket()
 
 int main()
 {
@@ -166,12 +199,17 @@ int main()
     // framegenerator object used in each test
     ofdmflexframe fg;
 
+    // framesynchronizer object used in each test
+    // TODO: Once we are using USRPs, move to an rx.c file that will run independently.
+    ofdmflexframesync fs;
+
     // Buffers for packet/frame data
-    char header[8];             // Must always be 8 bytes for ofdmflexframe
-    char payload[1000];         // Large enough to accomodate any (reasonable) payload that
-                                // the CE wants to use.
-    float complex frameSamples[10000];   // Large enough to accomodate any (reasonable) payload that 
-                                // the CE wants to use.
+    char header[8];                         // Must always be 8 bytes for ofdmflexframe
+    char payload[1000];                     // Large enough to accomodate any (reasonable) payload that
+                                            // the CE wants to use.
+    float complex frameSamples[10000];      // Buffer of frame samples for each symbol.
+                                            // Large enough to accomodate any (reasonable) payload that 
+                                            // the CE wants to use.
 
     // For each Cognitive Engine
     for (i_CE=0; i<NumCE; i_CE++)
@@ -191,6 +229,10 @@ int main()
             fg = CreateFG(ce, sc);  // Create ofdmflexframegen object with given parameters
                 //TODO: Initialize Connection to USRP                                     
 
+            // Initialize Receiver Defaults for current CE and Sc
+            // TODO: Once we are using USRPs, move to an rx.c file that will run independently.
+            fs = CreateFS(ce, sc);
+
             // Begin Testing Scenario
             DoneTransmitting = 0;
             while(!DoneTransmitting)
@@ -198,20 +240,23 @@ int main()
                 // Generate data to go into frame (packet)
                 txGeneratePacket(ce, &fg, header, payload);
                 // Simulate N tranmissions before simulating receiving them.
+                // i.e. Need to transmit each symbol in frame.
+                isLastSymbol = 0;
                 N = ce.iterations;
                 for (i_N= 0; i_N< N; i_N++)
                 {
                     // TODO: Create this function
-                    txTransmitPacket(ce, &fg);
+                    txTransmitPacket(ce, &fg, frameSamples);
                     // TODO: Create this function
                     //noiseAddNoise();
                     // TODO: Create this function
                     // Store a copy of the packet that was transmitted. For reference.
                     // txStoreTransmittedPacket();
                 
+                    // TODO: Once we are using USRPs, move to an rx.c file that will run independently.
+                    // Rx Receives packet
+                    rxReceivePacket(ce, &fs, frameSamples);
                 } // End Transmition For loop
-
-                // Rx Receives packet
 
                 // Receive and analyze data from rx
                     // Modify transmission parameters (in fg and in USRP) accordingly
